56ec153038400d3578e2b262c9a5516856cb35d5
samrfreitas <samuelrfreitassi@gmail.com>
2023-03-20 17:49:40 -0300

    feature: licitacoes routes

--- src/routes/licitacoes.js
+++ src/routes/licitacoes.js
@@ -1,13 +1,18 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const { getLicitacoes, getYears } = require('../Scrapper') 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  const json = {
-    'teste': 'Teste',
-    'dale': 'Chama Nenem',
-  }
-  res.status(200).json(json)
+router.post('/', async function (req, res, next) {
+  await getLicitacoes(req.body.data.year).then(licitacoes => {
+    res.status(200).json(licitacoes)
+  })
+})
+
+
+router.get('/years', async function(req, res, next) {
+  await getYears().then(years => {
+    res.status(200).json(years)
+  })
 })
 
 module.exports = router
